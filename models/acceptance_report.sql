144096284aec9a614bf89abd87131763e6056bd9
Mohammad Hussamuddin Arif <moham@Mac.fritz.box>
2025-01-19 14:22:18 +0100

    Updated acceptance_report transformations and documentation

--- models/acceptance_report.sql
+++ models/acceptance_report.sql
@@ -1,2 +1,15 @@
-SELECT *
+SELECT 
+    external_ref,
+    state,
+    CASE 
+        WHEN currency = 'MXN' THEN amount / JSON_EXTRACT(rates, '$.MXN')
+        WHEN currency = 'CAD' THEN amount / JSON_EXTRACT(rates, '$.CAD')
+        WHEN currency = 'EUR' THEN amount / JSON_EXTRACT(rates, '$.EUR')
+        WHEN currency = 'SGD' THEN amount / JSON_EXTRACT(rates, '$.SGD')
+        WHEN currency = 'AUD' THEN amount / JSON_EXTRACT(rates, '$.AUD')
+        WHEN currency = 'GBP' THEN amount / JSON_EXTRACT(rates, '$.GBP')
+        ELSE amount / JSON_EXTRACT(rates, '$.USD') -- Default to USD
+    END AS amount_usd,
+    currency,
+    DATE_FORMAT(STR_TO_DATE(date_time, '%Y-%m-%dT%H:%i:%sZ'), '%Y-%m-%d %H:%i:%s') AS date_time
 FROM {{ source('RevenueAnalytics', 'AcceptanceReport') }}
